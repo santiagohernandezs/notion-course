ef68cdd312654b62722ceec77fb3f1e12735a946
Tao <santiagooheernandez@gmail.com>
2023-12-12 12:28:20 -0400

    :art: Wip layout responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -13,7 +13,17 @@ export default {
         inter: ["Inter", "sans-serif"],
       },
       content: {
-        link: 'url("/public/link.svg")',
+        link: 'url("/link.svg")',
+      },
+      screens: {
+        "mobile/sm": "375px",
+        "mobile/md": "428px",
+        "tablet/sm": "768px",
+        "tablet/md": "834px",
+        "laptop/sm": "1280px",
+        "laptop/lg": "1440px",
+        "desktop/sm": "1600px",
+        "desktop/md": "1920px",
       },
     },
   },
