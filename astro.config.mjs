9fee3cf39f1ea11d13e3eb3de94cb7a47182d9ba
Tao <santiagooheernandez@gmail.com>
2023-12-13 13:06:16 -0400

    :bug: Switch to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,5 @@ import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
-  output: "hybrid",
   adapter: vercel(),
 });
