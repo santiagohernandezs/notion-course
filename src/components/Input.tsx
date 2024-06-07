095c60bf8ca3b15625145912b93e76c26c3823f9
Santiago Hernandez <santiagooheernandez@gmail.com>
2024-06-07 12:31:19 -0400

    🔥 Project updates
    Fast commit for demostration porpuses

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -15,31 +15,31 @@ export default function Input({ name, placeholder, icon, helperText }: Props) {
 	} = useFormContext()
 
 	const styles = {
-		input: cx(
-			'h-10 ring p-4 ring-gray-100 bg-gray-100 focus:bg-white-default transition-all ease-out duration-100 outline outline-[1px] outline-transparent focus:outline focus:outline-[1px] focus:outline-gray-300 rounded-md pl-10 bg-icon bg-[length:25px] bg-no-repeat',
-			{
-				'bg-[url("/src/components/icons/user.svg")]': icon === 'user',
-				'bg-[url("/src/components/icons/mail.svg")]': icon === 'mail'
-			},
-			{
-				'ring-red-400': errors[name],
-				'ring-blue-discord': dirtyFields[name] && !errors[name]
-			}
-		)
-	}
+    input: cx(
+      'h-10 ring p-4 ring-gray-100 bg-gray-100 focus:bg-white-default transition-all ease-out duration-100 outline outline-[1px] outline-transparent focus:outline focus:outline-[1px] focus:outline-gray-300 rounded-md pl-10 bg-icon bg-[length:25px] bg-no-repeat',
+      {
+        'bg-[url("/src/components/icons/user.svg")]': icon === 'user',
+        'bg-[url("/src/components/icons/mail.svg")]': icon === 'mail'
+      },
+      {
+        'ring-red-400': errors[name],
+        'ring-green-400': dirtyFields[name] && !errors[name]
+      }
+    )
+  }
 
-	return (
-		<div className='flex flex-col gap-2'>
-			<input {...register(name)} placeholder={placeholder} className={styles.input} />
-			<span>
-				{errors[name] ? (
-					<span className='text-sm font-normal text-red-500'>
-						{errors[name]?.message as string}
-					</span>
-				) : (
-					<span className='text-sm font-normal text-gray-500 opacity-80'>{helperText}</span>
-				)}
-			</span>
-		</div>
-	)
+  return (
+    <div className='flex flex-col gap-2'>
+      <input {...register(name)} placeholder={placeholder} className={styles.input} />
+      <span>
+        {errors[name] ? (
+          <span className='text-sm font-normal text-red-500'>
+            {errors[name]?.message as string}
+          </span>
+        ) : (
+          <span className='text-sm font-normal text-gray-500 opacity-80'>{helperText}</span>
+        )}
+      </span>
+    </div>
+  )
 }
--- src/pages/inscribete/Form.tsx
+++ src/pages/inscribete/Form.tsx
@@ -6,81 +6,85 @@ import cx from '../../utils/cx'
 import { sendData } from '../../utils/helpers'
 
 const schema = z.object({
-	name: z
-		.string()
-		.min(5, 'El nombre debe contener al menos 5 carateres')
-		.max(30, 'El nombre no puede exceder los 30 caracteres'),
-	email: z
-		.string()
-		.min(5, 'El correo debe contener al menos 5 caracteres')
-		.max(40, 'El correo no puede exceder los 40 caracteres')
-		.email('El correo debe ser un email válido')
+  name: z
+    .string()
+    .min(5, 'El nombre debe contener al menos 5 carateres')
+    .max(30, 'El nombre no puede exceder los 30 caracteres'),
+  email: z
+    .string()
+    .min(5, 'El correo debe contener al menos 5 caracteres')
+    .max(40, 'El correo no puede exceder los 40 caracteres')
+    .email('El correo debe ser un email válido')
 })
 
 type Inputs = z.infer<typeof schema>
 
 export default function Form() {
-	const methods = useForm<Inputs>({
-		resolver: zodResolver(schema)
-	})
+  const methods = useForm<Inputs>({
+    mode: 'all',
+    resolver: zodResolver(schema)
+  })
 
-	const {
-		handleSubmit,
-		formState: { isSubmitting, isSubmitSuccessful },
-		reset
-	} = methods
+  const {
+    handleSubmit,
+    formState: { isSubmitting, isSubmitSuccessful, isValid },
+    reset
+  } = methods
 
-	const styles = {
-		form: cx(
-			'flex flex-col w-[80%] tablet/sm:w-[50%] laptop/sm:w-1/4 h-auto rounded-xl gap-6 bg-white-default ring-1 p-7 ring-gray-200',
-			{
-				'ring-blue-discord ring-4': isSubmitSuccessful
-			}
-		),
-		button: cx(
-			'bg-black-text text-white-snow font-semibold rounded-lg font-inter gap-1 shadow-md text-base px-4 py-2',
-			{
-				'bg-black-text opacity-90': isSubmitting
-			}
-		)
-	}
+  const styles = {
+    form: cx(
+      'flex flex-col w-[80%] tablet/sm:w-[50%] laptop/sm:w-1/2 desktop/sm:w-2/5 h-auto rounded-xl gap-6 bg-white-default ring-1 p-7 ring-gray-200',
+      {
+        'ring-blue-discord ring-4': isSubmitSuccessful
+      }
+    ),
+    button: cx(
+      'bg-black-text text-white-snow font-semibold rounded-lg font-inter gap-1 shadow-md text-base px-4 py-2 trnasition-colors duration-150 cursor-pointer',
+      {
+        'bg-black-text opacity-90': isSubmitting
+      },
+      {
+        'bg-gray-500 cursor-not-allowed': !isValid
+      }
+    )
+  }
 
-	const onSubmit: SubmitHandler<Inputs> = async data => {
-		const { error, status } = await sendData(data)
+  const onSubmit: SubmitHandler<Inputs> = async data => {
+    const { error, status } = await sendData(data)
 
-		if (error) {
-			console.error('Error:', error)
-		} else {
-			reset()
-		}
-	}
+    if (error) {
+      console.error('Error:', error)
+    } else {
+      reset()
+    }
+  }
 
-	return (
-		<FormProvider {...methods}>
-			<div className={styles.form}>
-				<div className='flex flex-col gap-2 w-full'>
-					<h1 className='text-2xl font-bold text-center laptop/sm:text-3xl text-black-text'>
-						Inscríbete
-					</h1>
-					<p className='text-gray-500 text-center text-sm mobile/md:text-base'>
-						Regístrate para recibir información sobre nuestros productos y servicios
-					</p>
-				</div>
-				<form className='flex flex-col gap-4' onSubmit={handleSubmit(onSubmit)}>
-					<Input helperText='Tu nombre completo' name='name' placeholder='Nombre' icon='user' />
-					<Input helperText='Tu correo electrónico' name='email' placeholder='Email' icon='mail' />
+  return (
+    <FormProvider {...methods}>
+      <div className={styles.form}>
+        <div className='flex flex-col gap-2 w-full'>
+          <h1 className='text-2xl font-bold text-center laptop/sm:text-3xl text-black-text'>
+            Inscríbete
+          </h1>
+          <p className='text-gray-500 text-center text-sm mobile/md:text-base'>
+            Regístrate para recibir información sobre nuestros productos y servicios
+          </p>
+        </div>
+        <form className='flex flex-col gap-4' onSubmit={handleSubmit(onSubmit)}>
+          <Input helperText='Tu nombre completo' name='name' placeholder='Nombre' icon='user' />
+          <Input helperText='Tu correo electrónico' name='email' placeholder='Email' icon='mail' />
 
-					<button disabled={isSubmitting} type='submit' className={styles.button}>
-						{isSubmitting ? 'Enviando...' : 'Enviar'}
-					</button>
+          <button disabled={isValid} type='submit' className={styles.button}>
+            {isSubmitting ? 'Enviando...' : 'Enviar'}
+          </button>
 
-					{isSubmitSuccessful && (
-						<p className='text-blue-discord text-sm mx-auto text-center tablet/sm:text-left font-semibold'>
-							¡Gracias por inscribirte! Te contactaremos pronto.
-						</p>
-					)}
-				</form>
-			</div>
-		</FormProvider>
-	)
+          {isSubmitSuccessful && (
+            <p className='text-blue-discord text-sm mx-auto text-center tablet/sm:text-left font-semibold'>
+              ¡Gracias por inscribirte! Te contactaremos pronto.
+            </p>
+          )}
+        </form>
+      </div>
+    </FormProvider>
+  )
 }
